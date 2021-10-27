5a38bdeadb07c89c78e91bdcbf2a65a8c38d7a6b
greg <naudgr@gmail.com>
2021-10-27 23:49:31 +0200

    adding cont. about Us + its specific components

---
+++ src/Components/Concept.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Concept = () => (
+  <div className="concept">
+    <p>
+      {" "}
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas bibendum
+      suscipit dictum. Praesent tincidunt libero vitae eros pulvinar aliquet.
+      Nulla sodales felis vel velit viverra tempus. Sed aliquet diam sed tortor
+      rutrum maximus. Proin varius vulputate ipsum vel accumsan. Cras faucibus
+      nunc eget egestas lobortis. Class aptent taciti sociosqu ad litora
+      torquent per conubia nostra, per inceptos himenaeos. Nullam vel ante
+      vestibulum, vehicula nibh eu, dictum sem. Sed vitae erat fermentum,
+      elementum tortor nec, lacinia sem. Donec eu cursus ipsum. Nunc volutpat
+      dictum urna at suscipit. Sed sit amet efficitur lorem. Curabitur
+      scelerisque sed nibh vitae volutpat. Integer lacinia, mauris in bibendum
+      euismod, neque nunc luctus quam, quis pellentesque purus nisi sed tellus.
+      Phasellus eu condimentum leo. Quisque non congue urna. Vestibulum et
+      fringilla arcu. Vestibulum urna arcu, sollicitudin id porta sed, eleifend
+      congue urna. Vivamus vitae augue ac elit ultrices cursus. Integer sed
+      justo ac nibh efficitur congue id vitae lorem.{" "}
+    </p>
+  </div>
+);
+
+export default Concept;
