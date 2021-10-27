19f633b9c46e78fb8e4b2d084e7912b68bec4d8c
Vianney-Gln <vgeloen@gmail.com>
2021-10-27 16:23:22 +0200

    creation page category

--- src/Containers/Category.jsx
+++ src/Containers/Category.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import "../styles/category.css";
+import BackgroundImage from "../components/BackgroundImage";
 
-const Category = () => <div />;
+const Category = () => {
+  // définition de 30 divs
+  const nbrItems = 28;
+  const items = [];
 
+  for (let i = 0; i < nbrItems; i += 1) {
+    items.push(<div className="preview-items" />);
+  }
+
+  return (
+    <>
+      <BackgroundImage />
+      <div className="container-category">
+        {/** Ici composant NavBar */}
+
+        <div className="title-latest">
+          <h1>LATEST</h1>
+        </div>
+        <div className="container-previous-items">{items}</div>
+      </div>
+    </>
+  );
+};
 export default Category;
---
+++ src/components/BackgroundImage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function BackgroundImage() {
+  return (
+    <img
+      className="background-image"
+      src="http://image.tmdb.org/t/p/original/s3TBrRGB1iav7gFOCNx3H31MoES.jpg"
+      alt="background"
+    />
+  );
+}
+
+export default BackgroundImage;
