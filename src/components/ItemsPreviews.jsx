8821814fda0f05c80892a3770ad0a39d9f17f419
Vianney-Gln <vgeloen@gmail.com>
2021-10-27 20:07:48 +0200

    ajout des titres d'oeuvres sur un event mouseOver

--- src/components/ItemsPreviews.jsx
+++ src/components/ItemsPreviews.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/itemsPreviews.css";
 
 function ItemsPreviews() {
+  const [display, setDisplay] = useState(false);
+
+  function displayTitle() {
+    setDisplay(!display);
+  }
+
   return (
     <div className="preview-items">
       <img
+        onMouseOver={displayTitle}
+        onMouseLeave={displayTitle}
+        onFocus={0}
         src="http://image.tmdb.org/t/p/w300/s3TBrRGB1iav7gFOCNx3H31MoES.jpg"
         alt="films"
       />
-      <span>title</span>
+      {display && <span>title</span>}
     </div>
   );
 }
