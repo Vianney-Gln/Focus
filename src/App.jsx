fcbd48fcd22d429b4f840a3e89e315590feb21a7
Vianney-Gln <vgeloen@gmail.com>
2021-10-28 13:25:53 +0200

    mise à jour arborescence de fichier + modifs css sur itemsPreviews

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./styles/App.css";
-import Home from "./Containers/Home";
-import Suggestion from "./Containers/Suggestion";
-import PopupMenu from "./Containers/PopupMenu";
-import Category from "./Containers/Category";
-import AboutUs from "./Containers/AboutUs";
-import MyList from "./Containers/MyList";
-import SignUp from "./Containers/SignUp";
-import SignIn from "./Containers/SignIn";
-import ItemModal from "./Containers/ItemModal";
-import Player from "./Containers/Player";
+import Home from "./containers/Home";
+import Suggestion from "./containers/Suggestion";
+import PopupMenu from "./containers/PopupMenu";
+import Category from "./containers/Category";
+import AboutUs from "./containers/AboutUs";
+import MyList from "./containers/MyList";
+import SignUp from "./containers/SignUp";
+import SignIn from "./containers/SignIn";
+import ItemModal from "./containers/ItemModal";
+import Player from "./containers/Player";
 
 function App() {
   return (
--- src/components/ItemsPreviews.jsx
+++ src/components/ItemsPreviews.jsx
@@ -1,24 +1,16 @@
-import React, { useState } from "react";
 import "../styles/itemsPreviews.css";
+import React from "react";
 
 function ItemsPreviews() {
-  const [display, setDisplay] = useState(false);
-
-  function displayTitle() {
-    setDisplay(!display);
-  }
-
   return (
     <div className="preview-items">
-      <img
-        onMouseOver={displayTitle}
-        onMouseLeave={displayTitle}
-        onFocus={0}
-        src="http://image.tmdb.org/t/p/w300/s3TBrRGB1iav7gFOCNx3H31MoES.jpg"
-        alt="films"
-      />
-      {display && <span className="title">title</span>}
-      <span className="title-for-mobile">title</span>
+      <div className="container-image">
+        <img
+          src="http://image.tmdb.org/t/p/w300/s3TBrRGB1iav7gFOCNx3H31MoES.jpg"
+          alt="films"
+        />
+      </div>
+      <span className="title">title</span>
     </div>
   );
 }
