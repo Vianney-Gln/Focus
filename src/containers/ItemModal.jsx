f9aa9f2db0c225680638b4633dee9b9066143fee
gregN <naudgr@gmail.com>
2021-11-01 04:05:07 +0100

    +placehoder img, +jsx, +css
    adding westworld placeholder while ItemsPreviews issue is fixed, creating jsx, creating css

--- src/containers/ItemModal.jsx
+++ src/containers/ItemModal.jsx
@@ -1,5 +1,59 @@
 import React from "react";
 
-const ItemModal = () => <div />;
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// import ItemsPreviews from "../components/ItemsPreviews";
+// import StarRater from "../components/StarRater";
+import westworlded from "../assets/images/westworlded.jpg";
+// import "../styles/starRater.css";
+import "../styles/index.css";
+import "../styles/itemModal.css";
+import "../styles/itemsPreviews.css";
+
+const ItemModal = () => (
+  <div className="itemModal">
+    <div className="top-thumbnail">
+      {/* <ItemsPreviews /> */}
+      <img src={westworlded} alt="" />
+      <h1>eternal sunshine of the spotless mind</h1>
+      <button className="button-close" type="button">
+        X
+      </button>
+    </div>
+    <div className="bottom-infos">
+      <div className="bottom-infos-grid">
+        <div className="bottom-infos-grid-creators">
+          Lisa Joy & Jonathan Nolan
+        </div>
+        <div className="bottom-infos-grid-date">2016</div>
+        <div className="bottom-infos-grid-length">3 Seasons</div>
+        <div className="bottom-infos-grid-starRater">{/* <StarRater /> */}</div>
+        <div className="bottom-infos-grid-availableOn">available on</div>
+        <div className="bottom-infos-grid-addToMyList">
+          <button className="btn-common" type="button">
+            <i className="far fa-plus-square" />
+            add to My List
+          </button>
+        </div>
+        <div className="bottom-infos-grid-platforms">
+          WetFix AmaClone ThePirateBay
+        </div>
+        <div className="bottom-infos-grid-synopsis">
+          In the 2050s, Delos Inc. operates several theme parks, including the
+          American-Old-West-themed Westworld. Each environment is populated by
+          hosts. Indistinguishable from humans, these androids are programmed to
+          fulfill the guests every desire. They will engage in—and be the
+          objects of—every kind of violent and/or sexual activity, but their
+          programming makes it impossible for them to allow the guests to be
+          harmed. The Operators create narratives for these hosts to repeat each
+          day while interacting with guests, but they wipe their memories each
+          cycle. Delos Inc. party line is that the android hosts, being
+          machines, can not be harmed by these scenarios the same way that a
+          human would be.{" "}
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default ItemModal;
