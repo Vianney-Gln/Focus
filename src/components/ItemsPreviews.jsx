2366c7ab85464d8f070f555ea82965d7ac691f19
Vianney-Gln <vgeloen@gmail.com>
2021-11-02 18:42:25 +0100

    création d'un composant ImagePreviewItem + suppression du module hamburgers inutile

--- src/components/ItemsPreviews.jsx
+++ src/components/ItemsPreviews.jsx
@@ -1,14 +1,12 @@
 import "../styles/itemsPreviews.css";
 import React from "react";
+import ImageItemPreviews from "./ImageItemPreviews";
 
 function ItemsPreviews() {
   return (
     <div className="preview-items">
       <div className="container-image">
-        <img
-          src="http://image.tmdb.org/t/p/w300/s3TBrRGB1iav7gFOCNx3H31MoES.jpg"
-          alt="films"
-        />
+        <ImageItemPreviews />
       </div>
       <span className="title">title</span>
     </div>
