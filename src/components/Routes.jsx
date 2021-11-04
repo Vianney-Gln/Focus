f754865d61a50416abc15aacb7c9f7ca403ea3a8
Vianney-Gln <vgeloen@gmail.com>
2021-11-04 11:29:06 +0100

    creation composant routes

---
+++ src/components/Routes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "../containers/Home";
+import Category from "../containers/Category";
+import AboutUs from "../containers/AboutUs";
+import MyList from "../containers/MyList";
+
+function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/category" component={Category} />
+      <Route exact path="/aboutus" component={AboutUs} />
+      <Route exact path="/aboutus" component={MyList} />
+    </Switch>
+  );
+}
+
+export default Routes;
