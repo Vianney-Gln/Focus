8deec545a38a97fba0d6239e5ed8dcb875ecce94
KevinDurand974 <k.durand.974@gmail.com>
2021-11-04 15:12:34 +0100

    fix missplacement of popupmenu

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -4,12 +4,10 @@ import Home from "../containers/Home";
 import Category from "../containers/Category";
 import AboutUs from "../containers/AboutUs";
 import MyList from "../containers/MyList";
-import PopupMenu from "../containers/PopupMenu";
 
 function Routes() {
   return (
     <>
-      <PopupMenu />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/category" component={Category} />
