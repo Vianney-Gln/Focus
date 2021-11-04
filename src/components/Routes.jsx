769226d72c0c40fec84bf1f68580c56087bed1fe
Vianney-Gln <vgeloen@gmail.com>
2021-11-04 13:53:02 +0100

    route logomobile ok

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -4,15 +4,19 @@ import Home from "../containers/Home";
 import Category from "../containers/Category";
 import AboutUs from "../containers/AboutUs";
 import MyList from "../containers/MyList";
+import PopupMenu from "../containers/PopupMenu";
 
 function Routes() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/category" component={Category} />
-      <Route exact path="/aboutus" component={AboutUs} />
-      <Route exact path="/mylist" component={MyList} />
-    </Switch>
+    <>
+      <PopupMenu />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/category" component={Category} />
+        <Route exact path="/aboutus" component={AboutUs} />
+        <Route exact path="/mylist" component={MyList} />
+      </Switch>
+    </>
   );
 }
 
