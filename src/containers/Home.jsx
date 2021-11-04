2699186dbc1b5b9a2191f7985047bcb47ab2b1f2
KevinDurand974 <k.durand.974@gmail.com>
2021-11-04 17:04:17 +0100

    update css

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hamburger from "hamburger-react";
 import { Link } from "react-router-dom";
 import Logo from "../components/Logo";
+import LogoMobile from "../components/LogoMobile";
 import SearchBar from "../components/SearchBar";
 import SwitchMtoS from "../components/Switch";
 import Suggestion from "./Suggestion";
@@ -10,29 +11,29 @@ import "../styles/home.scss";
 
 const Home = () => (
   <div className="Containerhome">
+    <header className="navBar">
+      <div className="navFixe">
+        <Logo />
+        <LogoMobile />
+        <SearchBar />
+      </div>
+      <div className="navunfixe">
+        <div className="contbuttonmylist">
+          <Link to="/mylist" className="buttonmylist">
+            MY LIST
+          </Link>
+        </div>
+        <div className="contButtonSignIn">
+          <button type="button" className="btn-common btn-aperture">
+            SIGN IN
+          </button>
+        </div>
+      </div>
+      <div className="fakeBurger">
+        <Hamburger />
+      </div>
+    </header>
     <div className="home">
-      <header className="navBar">
-        <div className="navFixe">
-          <Logo />
-          <SearchBar />
-        </div>
-        <div className="navunfixe">
-          <div className="contbuttonmylist">
-            <Link to="/mylist" className="buttonmylist">
-              MY LIST
-            </Link>
-          </div>
-          <div className="contButtonSignIn">
-            <button type="button" className="btn-common btn-aperture">
-              SIGN IN
-            </button>
-          </div>
-        </div>
-        <div className="fakeBurger">
-          <Hamburger />
-        </div>
-      </header>
-
       <div className="mainHome">
         <ul className="navhome">
           <li>
@@ -56,6 +57,8 @@ const Home = () => (
         <SwitchMtoS />
       </div>
     </div>
+    <Suggestion />
+    <Suggestion />
     <Suggestion />
     <div className="footer">
       <ul className="footercategory">
