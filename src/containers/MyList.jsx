ff1329180cdcbae41b6a78d7f57a48fad82cef9e
Vianney-Gln <vgeloen@gmail.com>
2021-11-06 13:09:29 +0100

    commencement du sprint d'affichage de la popupMenu depuis myList

--- src/containers/MyList.jsx
+++ src/containers/MyList.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import { React, useState } from "react";
 import Hamburger from "hamburger-react";
 import Logo from "../components/Logo";
+import PopupMenu from "./PopupMenu";
 import "../styles/myList.css";
 import ElementList from "../components/ElementList";
 
 /* structure du composant myList */
 const MyList = () => {
+  // state pour l'état du menu popup
+  const [openMenu, setOpenMenu] = useState(false);
+
   /* creation d'une boucle pour appeler plusieurs fois le composant <ElementList /> */
 
   const itemLists = [];
@@ -15,28 +19,33 @@ const MyList = () => {
   }
 
   return (
-    <div className="myList">
-      <div className="barre-logo-burger">
-        <Logo />
-        <Hamburger />
-      </div>
-
-      <div className="container-views-titles">
-        <div className="views-titles">
-          <div className="viewed-asc">
-            <button className="viewed btn-aperture" type="button">
-              Viewed
-            </button>
-            <button type="button" className="button-margin-left btn-aperture">
-              Asc
-            </button>
-          </div>
-          <h1>MY LIST</h1>
+    <>
+      {openMenu && <PopupMenu />}
+      <div className="myList">
+        <div className="barre-logo-burger">
+          <Logo />
+          <Hamburger
+            onToggle={() => setTimeout(() => setOpenMenu(true), 100)}
+          />
         </div>
-      </div>
 
-      <div className="container-items-list">{itemLists}</div>
-    </div>
+        <div className="container-views-titles">
+          <div className="views-titles">
+            <div className="viewed-asc">
+              <button className="viewed btn-aperture" type="button">
+                Viewed
+              </button>
+              <button type="button" className="button-margin-left btn-aperture">
+                Asc
+              </button>
+            </div>
+            <h1>MY LIST</h1>
+          </div>
+        </div>
+
+        <div className="container-items-list">{itemLists}</div>
+      </div>
+    </>
   );
 };
 export default MyList;
