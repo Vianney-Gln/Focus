9e38b092790dda68cf97cb692116181daf26c416
Vianney-Gln <vgeloen@gmail.com>
2021-11-06 11:44:25 +0100

    suppression du fichier route et routing déplacer dans app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import "./styles/App.css";
+import { Switch, Route } from "react-router-dom";
 // import PopupMenu from "./containers/PopupMenu";
-import Routes from "./components/Routes";
+import Home from "./containers/Home";
+import Category from "./containers/Category";
+import AboutUs from "./containers/AboutUs";
+import MyList from "./containers/MyList";
 
 function App() {
   return (
     <>
       {/* <PopupMenu /> */}
-      <Routes />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/category" component={Category} />
+        <Route exact path="/aboutus" component={AboutUs} />
+        <Route exact path="/mylist" component={MyList} />
+      </Switch>
     </>
   );
 }
