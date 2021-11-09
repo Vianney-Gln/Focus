61d6f6cac43af4c8a2cc504db732513de8f36cd0
gregN <naudgr@gmail.com>
2021-11-09 16:45:01 +0100

    fix responsive, now generic tablet + iphone5, fix burger position

--- src/containers/AboutUs.jsx
+++ src/containers/AboutUs.jsx
@@ -1,93 +1,97 @@
 import React, { useContext } from "react";
 import { Logo, LogoMobile } from "../components";
+import BurgerContext from "../contexts/BurgerContext";
 import avatarK from "../assets/images/avatarK.png";
 import avatarG from "../assets/images/avatarG.png";
 import avatarL from "../assets/images/avatarL.png";
 import avatarV from "../assets/images/avatarV.png";
 import reactIconHeart from "../assets/images/reactIconHeart.png";
-import BurgerContext from "../contexts/BurgerContext";
 
 import "../styles/index.css";
 import "../styles/aboutUs.css";
 
-const AboutUs = () => {
+export default function AboutUs() {
   // utilisation du contexte pour garder le burger affiché même au rechargement de la page
   const burgerContext = useContext(BurgerContext);
   burgerContext.displayBurger();
 
   return (
     <div className="aboutUs">
-    <header className="navBar">
-      <div className="navFixe">
-        <Logo />
-        <LogoMobile />
-      </div>
-    </header>
-    <div className="below-header">
-      <h1>Concept</h1>
-      <div className="concept">
-        <p>
-          How to get away from the principle of giant streaming platforms like
-          Netflix or Youtube? How would it be, as viewers, to NOT be overwhelmed
-          by tons of suggestions all around. To no longer consume cinema as the
-          mass product it became ? What would be a local movie theater, in a
-          digital shape, suggesting a few cinematographic works to discover ?
-        </p>
-        <p className="tagline">Dear reader & viewer, answer is ours & yours.</p>
-      </div>
-      <h1>by</h1>
-      <div className="teamMembers">
-        <div className="teamMembers-step1">
-          <a href="https://github.com/KevinDurand974">
-            <p className="member-name">Kévin D.</p>
-          </a>
-          <a href="https://github.com/KevinDurand974">
-            <img src={avatarK} alt="avatar" />
-          </a>
-          <p>Fix them all medkit </p>
-          <p className="quote-text">« Unity in numbers »</p>
+      <header className="navBar">
+        <div className="navFixe">
+          <Logo />
+          <LogoMobile />
         </div>
-        <div className="teamMembers-step2">
-          <a href="https://github.com/anarkhya">
-            <p className="member-name">Greg N.</p>
-          </a>
-          <a href="https://github.com/anarkhya">
-            <img src={avatarG} alt="avatar" />
-          </a>
-          <p>console.log(philosophy) </p>
-          <p className="quote-text">« !maybe && maybe !not. »</p>
+      </header>
+      <main className="below-header">
+        <h1>Concept</h1>
+        <div className="concept">
+          <p>
+            How to get away from the principle of giant streaming platforms like
+            Netflix or Youtube? How would it be, as viewers, to NOT be
+            overwhelmed by tons of suggestions all around. To no longer consume
+            cinema as the mass product it became ? What would be a local movie
+            theater, in a digital shape, suggesting a few cinematographic works
+            to discover ?
+          </p>
+          <p className="tagline">
+            Dear reader & viewer, answer is ours & yours.
+          </p>
         </div>
-        <div className="teamMembers-step3">
-          <a href="https://github.com/LeonieNollevalle">
-            <p className="member-name">Léonie N.</p>{" "}
-          </a>
-          <a href="https://github.com/LeonieNollevalle">
-            <img src={avatarL} alt="avatar" />
-          </a>
-          <p>Typographically anxious </p>
-          <p className="quote-text">« This is good bail, for now. »</p>
+        <h1>by</h1>
+        <div className="flex-teamMembers">
+          <div className="teamMembers">
+            <div className="teamMembers-step1">
+              <a href="https://github.com/KevinDurand974">
+                <p className="member-name">Kévin D.</p>
+              </a>
+              <a href="https://github.com/KevinDurand974">
+                <img src={avatarK} alt="avatar" />
+              </a>
+              <p>Fix them all medkit </p>
+              <p className="quote-text">« Unity in numbers »</p>
+            </div>
+            <div className="teamMembers-step2">
+              <a href="https://github.com/anarkhya">
+                <p className="member-name">Greg N.</p>
+              </a>
+              <a href="https://github.com/anarkhya">
+                <img src={avatarG} alt="avatar" />
+              </a>
+              <p>console.log(philosophy) </p>
+              <p className="quote-text">« !maybe && maybe !not. »</p>
+            </div>
+            <div className="teamMembers-step3">
+              <a href="https://github.com/LeonieNollevalle">
+                <p className="member-name">Léonie N.</p>{" "}
+              </a>
+              <a href="https://github.com/LeonieNollevalle">
+                <img src={avatarL} alt="avatar" />
+              </a>
+              <p>Typographically anxious </p>
+              <p className="quote-text">« This is good bail, for now. »</p>
+            </div>
+            <div className="teamMembers-step4">
+              <a href="https://github.com/Vianney-Gln">
+                <p className="member-name">Vianney G.</p>{" "}
+              </a>
+              <a href="https://github.com/Vianney-Gln">
+                <img src={avatarV} alt="avatar" />
+              </a>
+              <p>ESLint premium member </p>
+              <p className="quote-text">« Failed to compile tagline. »</p>
+            </div>
+          </div>
         </div>
-        <div className="teamMembers-step4">
-          <a href="https://github.com/Vianney-Gln">
-            <p className="member-name">Vianney G.</p>{" "}
-          </a>
-          <a href="https://github.com/Vianney-Gln">
-            <img src={avatarV} alt="avatar" />
-          </a>
-          <p>ESLint premium member </p>
-          <p className="quote-text">« Failed to compile tagline. »</p>
+        <div className="minitext">
+          <p>...and lots of </p>
+          <img
+            className="icon-react-cute"
+            src={reactIconHeart}
+            alt="icon react"
+          />
         </div>
-      </div>
-      <div className="minitext">
-        <p>...and lots of </p>
-        <img
-          className="icon-react-cute"
-          src={reactIconHeart}
-          alt="icon react"
-        />
-      </div>
+      </main>
     </div>
   );
-};
-
-export default AboutUs;
+}
