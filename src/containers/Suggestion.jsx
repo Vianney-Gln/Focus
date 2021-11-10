6e348cf2a438200b0bf947ebe9d0e7276d4479f7
Vianney-Gln <vgeloen@gmail.com>
2021-11-10 12:20:47 +0100

    tab fix

--- src/containers/Suggestion.jsx
+++ src/containers/Suggestion.jsx
@@ -40,11 +40,11 @@ const Suggestion = ({ refValue }) => {
                 <i className="icon-left-open-big" />
               </button>
               <div className="container-image-player">
-               <img className="img-slideshow" src={SlideImg} alt="slideimg" />
-               <button type="button" className="btn-fleche-slideShow">
-                <i className="icon-play" />
-              </button>
-            </div>
+                <img className="img-slideshow" src={SlideImg} alt="slideimg" />
+                <button type="button" className="btn-fleche-slideShow">
+                  <i className="icon-play" />
+                </button>
+              </div>
               <button type="button" className="btn-fleche-slideShow">
                 <i className="icon-right-open-big" />
               </button>
