38a22c7768ae756a9758304971a1659a99135ff7
Vianney-Gln <vgeloen@gmail.com>
2021-11-15 19:39:39 +0100

    creation d'un fichier ModalContext.jsx

---
+++ src/contexts/ModalContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState } from "react";
+
+const ModalContext = createContext(null);
+
+function ModalProvider({ children }) {
+  // Modal Toggle Open/Closed
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const setModalIsOpenToTrue = () => {
+    setModalIsOpen(true);
+  };
+  const setModalIsOpenToFalse = () => {
+    setModalIsOpen(false);
+  };
+  return (
+    <ModalContext.Provider
+      value={(modalIsOpen, setModalIsOpenToFalse, setModalIsOpenToTrue)}
+    >
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+export { ModalProvider, ModalContext };
