948809a7fab73b3f7f8dc8d9ba0853f0e1ccc1a9
KDurand974 <k.durand.974@gmail.com>
2021-11-16 14:08:13 +0100

    new test of logged user

--- src/containers/MyList.jsx
+++ src/containers/MyList.jsx
@@ -8,19 +8,19 @@ import { getListofMyList } from "../services/FirebaseRealtimeDatabase";
 
 /* structure du composant myList */
 const MyList = () => {
+  // Pour les redirections
   const history = useHistory();
-  // utilisation du contexte pour garder le burger affiché même au rechargement de la page
-  const burgerContext = useContext(BurgerContext);
-  // burgerContext.displayBurger();
 
+  // useContext des différents context
+  const burgerContext = useContext(BurgerContext);
   const authContext = useContext(AuthContext);
-  useEffect(() => {
-    if (!authContext.isLogged) {
-      history.push("/");
-    } else {
-      burgerContext.displayBurger();
-    }
-  }, []);
+
+  // Si page chargé mais pas connecté
+  if (!authContext.isLogged) {
+    history.push("/");
+  } else {
+    burgerContext.displayBurger();
+  }
 
   /* creation d'une boucle pour appeler plusieurs fois le composant <ElementList /> */
   const [itemList, setItemList] = useState([]);
@@ -34,45 +34,34 @@ const MyList = () => {
     })();
   }, []);
 
-  /* creation d'une boucle pour appeler plusieurs fois le composant <ElementList /> */
-  // const itemLists = [];
-  // for (let i = 0; i < 10; i += 1) {
-  //   itemLists.push(<ElementList />);
-  // }
-
   return (
     <>
-      {authContext.isLogged && (
-        <div className="myList">
-          <div className="barre-logo-burger">
-            <Logo />
-            <SearchBar />
-          </div>
-          <div className="barre-logo-burger-mobile">
-            <LogoMobile />
-            <SearchBar />
-          </div>
-
-          <div className="container-views-titles">
-            <div className="views-titles">
-              <div className="viewed-asc">
-                <button className="viewed btn-aperture" type="button">
-                  Viewed
-                </button>
-                <button
-                  type="button"
-                  className="button-margin-left btn-aperture"
-                >
-                  Asc
-                </button>
-              </div>
-              <h1>MY LIST</h1>
-            </div>
-          </div>
-
-          <div className="container-items-list">{itemList}</div>
+      <div className="myList">
+        <div className="barre-logo-burger">
+          <Logo />
+          <SearchBar />
         </div>
-      )}
+        <div className="barre-logo-burger-mobile">
+          <LogoMobile />
+          <SearchBar />
+        </div>
+
+        <div className="container-views-titles">
+          <div className="views-titles">
+            <div className="viewed-asc">
+              <button className="viewed btn-aperture" type="button">
+                Viewed
+              </button>
+              <button type="button" className="button-margin-left btn-aperture">
+                Asc
+              </button>
+            </div>
+            <h1>MY LIST</h1>
+          </div>
+        </div>
+
+        <div className="container-items-list">{itemList}</div>
+      </div>
     </>
   );
 };
