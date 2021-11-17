ab354103d42ce400265dab14b2610056cd8d5607
KDurand974 <k.durand.974@gmail.com>
2021-11-17 12:01:10 +0100

    add functionnality of add / remove to mylist, set a rate in itemModal

--- src/containers/ItemModal.jsx
+++ src/containers/ItemModal.jsx
@@ -1,9 +1,15 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Modal from "react-modal";
 import { Rating } from "react-simple-star-rating";
 import { SignContext } from "../contexts/SignContext";
 import { AuthContext } from "../contexts/AuthContext";
-import { updateMovie } from "../services/FirebaseRealtimeDatabase";
+import {
+  addMovie,
+  addMovieToMyList,
+  removeFromMyList,
+  updateUserMyList,
+  getListofMyList,
+} from "../services/FirebaseRealtimeDatabase";
 import { ModalContext } from "../contexts/ModalContext";
 // import { ItemsPreviews } from "../components";
 // import westworlded from "../assets/images/westworlded.jpg";
@@ -26,26 +32,6 @@ const ItemModal = () => {
     ));
   }
 
-  console.log(icones);
-
-  const handleAddToMyList = async () => {
-    try {
-      if (authContext.isLogged) {
-        // Ajouter a MaList
-        await updateMovie(460458);
-      } else {
-        // Demander de se connecter
-        signinContext.showSignIn();
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  // si connecté, enregistrer le rating
-  // sinon pas enregistrer
-  const handleRating = (value) => {
-    setRating(value);
-  };
   /* Modal Toggle Open/Closed
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const setModalIsOpenToTrue = () => {
@@ -56,6 +42,112 @@ const ItemModal = () => {
   };
   CE CODE EST DANS MODALCONTEXT
   */
+
+  /**
+   * Get user Mylist if Connected and format to get only ID
+   */
+  const [userMovieMyListID, setUserMovieMyListID] = useState([]);
+  useEffect(() => {
+    (async () => {
+      if (!authContext.isLogged) return;
+      const userMyList = await getListofMyList(authContext.userID);
+      setUserMovieMyListID(userMyList.map((movie) => movie.id));
+    })();
+  }, []);
+
+  /**
+   * State who set the text in Button Add / Remove to MyList
+   */
+  const [buttonType, setButtonType] = useState(null);
+
+  /**
+   * Change the button text
+   * @param {boolean} bool If add or remove but in Boolean
+   */
+  const buttonMyList = (bool) => {
+    // Si ajouté
+    if (bool) {
+      setButtonType(
+        <>
+          <i className="icon-plus" /> Add to my list
+        </>
+      );
+    } else {
+      setButtonType(
+        <>
+          <i className="icon-cancel" /> Remove
+          <br />
+          from my list
+        </>
+      );
+    }
+  };
+
+  /**
+   * Add the movie to user list if connected
+   */
+  const handleAddToMyList = async () => {
+    try {
+      // Ajouter a MaList
+      if (authContext.isLogged) {
+        // try to add this movie to the database
+        const movieData = {
+          title: modalContext.infosMovie.title || "Not documented",
+          poster: modalContext.infosMovie.background || "Not documented",
+          author: modalContext.infosMovie.author || "Not documented",
+          date: modalContext.infosMovie.date.base || "Not documented",
+          duration: modalContext.infosMovie.duration.base || "Not documented",
+        };
+        await addMovie(modalContext.infosMovie.id, movieData);
+        // update to add user on movie
+        await addMovieToMyList(authContext.userID, modalContext.infosMovie.id);
+        // Change button to "Remove from my list"
+        buttonMyList(false);
+      } else {
+        // Demander de se connecter
+        signinContext.showSignIn();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /**
+   * Remove the movie from the user list if connected
+   */
+  const handleRemoveOfMyList = async () => {
+    try {
+      // Remove movie from MaList
+      if (authContext.isLogged) {
+        await removeFromMyList(authContext.userID, modalContext.infosMovie.id);
+        // Change button to "Add to my list"
+        buttonMyList(true);
+      } else {
+        // Demander de se connecter
+        signinContext.showSignIn();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /**
+   * Handle the rating of a movie but save only if connected
+   * @param {number} value Rating number /5
+   * @param {number} movieID Movie ID
+   * @returns Because eslint will not work without this
+   */
+  const handleRating = (value, movieID) => {
+    setRating(value);
+    // si connecter enregistrer le rating
+    if (authContext.isLogged) {
+      // Connected
+      updateUserMyList(authContext.userID, movieID, {
+        rating: value,
+      });
+    }
+    return true;
+  };
 
   return (
     <div className="itemModal">
@@ -104,13 +196,36 @@ const ItemModal = () => {
                 Available On <i className="fa fa-play-circle-o" />
               </div>
               <div className="bottom-infos-grid-addToMyList">
-                <button
-                  className="btn-addToMyList"
-                  type="button"
-                  onClick={handleAddToMyList}
-                >
-                  <i className="icon-plus" /> Add to my list
-                </button>
+                {userMovieMyListID.includes(modalContext.infosMovie.id) && (
+                  <button
+                    className="btn-addToMyList"
+                    type="button"
+                    onClick={handleRemoveOfMyList}
+                  >
+                    {buttonType !== null && buttonType}
+                    {buttonType === null && (
+                      <>
+                        <i className="icon-cancel" /> Remove
+                        <br />
+                        from my list
+                      </>
+                    )}
+                  </button>
+                )}
+                {!userMovieMyListID.includes(modalContext.infosMovie.id) && (
+                  <button
+                    className="btn-addToMyList"
+                    type="button"
+                    onClick={handleAddToMyList}
+                  >
+                    {buttonType !== null && buttonType}
+                    {buttonType === null && (
+                      <>
+                        <i className="icon-plus" /> Add to my list
+                      </>
+                    )}
+                  </button>
+                )}
               </div>
               <div className="bottom-infos-grid-platforms">{icones}</div>
               <div className="bottom-infos-grid-synopsis">
