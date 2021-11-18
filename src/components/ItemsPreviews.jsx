89602af0fc5ba937bd58f60369355d9b550075c9
LeonieNollevalle <leonie.nollevalle@gmail.com>
2021-11-18 21:59:20 +0100

    indentation css + suppression de code en commentaire dans itemPreview

--- src/components/ItemsPreviews.jsx
+++ src/components/ItemsPreviews.jsx
@@ -19,12 +19,7 @@ function ItemsPreviews({ data, id }) {
       <div className="container-image">
         <ImageItemPreviews source={data.image ? data.image : defaultImg} />
       </div>
-      <span className="title">
-        {/* {data.title.length > 18
-          ? `${data.title.substring(0, 12)}...`
-          : data.title} */}
-        {data.title}
-      </span>
+      <span className="title">{data.title}</span>
     </div>
   );
 }
