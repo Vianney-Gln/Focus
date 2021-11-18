508e52807656187a88253bcab5ccc72ee84a08f9
LeonieNollevalle <leonie.nollevalle@gmail.com>
2021-11-18 21:40:12 +0100

    ajout de commentaire de fonction

--- src/components/ButtonSignIn.jsx
+++ src/components/ButtonSignIn.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 const ButtonSignIn = () => (
   <div className="contButtonSignIn">
-    {" "}
     <button type="button" className="btn-common btn-aperture">
       SIGN IN
     </button>
